08e023586b66620a629d373e43a0cc77b223ada3
RasmusBruhn <rasmusbruhnnielsen@gmail.com>
2022-06-16 17:30:36 +0200

    Started defining the functions

--- Files.h
+++ Files.h
@@ -1,4 +1,24 @@
 #ifndef FILES_H_DEFINED
 #define FILES_H_DEFINED
 
+#include <stdio.h>
+
+// Errors
+#define ERR_PREFIX FIL
+#include <Error.h>
+
+enum _SET_ErrorID {
+    SET_ERRORID_NONE = 0x300000000
+};
+
+#define SET_ERRORMES_MALLOC "Unable to allocate memory"
+
+// Reads a segment of a file
+// File: The file to load from
+char *_FIL_LoadSegment(FILE File, size_t Start, size_t Size);
+
+char *FIL_LoadSplit(char *FileName, size_t MaxSize);
+
+char *FIL_Load(char *FileName);
+
 #endif
